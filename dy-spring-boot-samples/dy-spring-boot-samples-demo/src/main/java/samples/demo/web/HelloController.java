72ed91f807563e683a4e302d14531587b776c482
differentialmanifold <differentialmanifold@gmail.com>
2021-02-06 16:02:08 +0800

    add logback

--- dy-spring-boot-samples/dy-spring-boot-samples-demo/src/main/java/samples/demo/web/HelloController.java
+++ dy-spring-boot-samples/dy-spring-boot-samples-demo/src/main/java/samples/demo/web/HelloController.java
@@ -1,12 +1,17 @@
 package samples.demo.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @RequestMapping("/hello")
     public String hello() {
+        logger.info("hello world");
         return "Hello World";
     }
 
